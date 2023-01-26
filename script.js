b2b702c9f66ae9fa7ddfb714b0b729eba957438f
Samuel Kuta <kuta@SPSEJECNANET.CZ>
2023-01-26 09:10:22 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
-draw();
+const canvas = document.getElementById("canvas");
+const pen = canvas.getContext("2d");
 
-function draw(){
-    const canvas = document.getElementById("canvas");
+const buttons = {};
+
+buttons.thiccBrush = document.querySelector("#thiccBrush");
+buttons.brush = document.querySelector("#brush");
+
+
+let startDrawing = false;
+let lineWidth = 5;
+
+canvas.addEventListener('onclick',function(e){
+    startDrawing = true;
+    
+});
+
+canvas.addEventListener('onmouseup',function(e){
+    console.log("Mouse up fired in canvas.");
+    
+});
+
+
+canvas.addEventListener('mousemove',function(e){
+    
+    
+    if(e.buttons != 1){startDrawing= false; return }
+
+    
+    let x = e.clientX-canvas.offsetLeft;
+    let y = e.clientY-canvas.offsetTop;    
+    console.log("X="+x+"  Y="+y);
+    draw(x,y,startDrawing,15,'#ff0000');
+    
+});
+
+
+//draw();
+
+function draw(x,y,started,lineWidth,color){
+    
     if(canvas.getContext){
-        const pen = canvas.getContext("2d");
 
-        pen.beginPath();
-        pen.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-        pen.moveTo(110, 75);
-        pen.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-        pen.moveTo(65, 65);
-        pen.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-        pen.moveTo(95, 65);
-        pen.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+        
+        pen.strokeStyle = color;
+        pen.lineWidth = lineWidth;
+        
+
+        if(!startDrawing){
+            startDrawing = true;
+            pen.beginPath()
+            pen.moveTo(x,y);
+            return;
+        }
+        pen.lineTo(x, y);
         pen.stroke();
+       
     }
 }
